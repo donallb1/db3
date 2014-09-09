8f2d492b00c79cdca49fc6a40117de96fdeb8387
donallb1 <donallb1@student.op.ac.nz>
2014-09-09 23:34:00 +0000

    Edited user controller

--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -1,36 +1,7 @@
-class User < ActiveRecord::Base
-	
-	####################################################################
-	###############             User Model           ###################
-	####################################################################
-	
-	has_many :splatts
-	
-	has_and_belongs_to_many :follows,
-		class_name: "User",
-		join_table: :follows,
-		foreign_key: :follower_id,
-		association_foreign_key: :followed_id
-		
-	has_and_belongs_to_many :followed_by,
-		class_name: "User",
-		join_table: :follows,
-		foreign_key: :followed_id,
-		association_foreign_key: :follower_id
-	
-	#Must enter a name in the name feild
-	validates :name, presence: true
-	
-	#emil must be unique, no two users can have the same email
-	validates :email, uniqueness: {case_sensitive: false}
-	
-	#
-#	validates :password, length: {minimum: 8}, if: :strong?
-	
-	def strong?
-		password =~ /.*\d+.*/ && \
-		password =~ /.*[a-z]+.*/ && \
-		password =~ /.*[A-Z].*/
-	end
-	
+class User
+  include Mongoid::Document
+  field :name, type: String
+  field :email, type: String
+  field :password, type: String
+  field :blurb, type: String
 end
