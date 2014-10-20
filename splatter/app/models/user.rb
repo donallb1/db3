7ea2c1eda75ae549663c36150b3e4c1d20c46730
donallb1 <donallb1@student.op.ac.nz>
2014-10-20 01:28:51 +0000

    Finished lab 12.1

--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -1,36 +1,8 @@
-class User < ActiveRecord::Base
-	
-	####################################################################
-	###############             User Model           ###################
-	####################################################################
-	
-	has_many :splatts
-	
-	has_and_belongs_to_many :follows,
-		class_name: "User",
-		join_table: :follows,
-		foreign_key: :follower_id,
-		association_foreign_key: :followed_id
-		
-	has_and_belongs_to_many :followed_by,
-		class_name: "User",
-		join_table: :follows,
-		foreign_key: :followed_id,
-		association_foreign_key: :follower_id
-	
-	#Must enter a name in the name feild
-	validates :name, presence: true
-	
-	#emil must be unique, no two users can have the same email
-	validates :email, uniqueness: {case_sensitive: false}
-	
-	#
-#	validates :password, length: {minimum: 8}, if: :strong?
-	
-	def strong?
-		password =~ /.*\d+.*/ && \
-		password =~ /.*[a-z]+.*/ && \
-		password =~ /.*[A-Z].*/
-	end
-	
+class User < Hashie::Dash
+	property :email
+	property :name
+	property :password
+	property :blurb
+	property :follows
+	property :followers
 end
